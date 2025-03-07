15b5f4b840796a83791b47928fcafe783816bc2f
helix.song <helix.song@vinotech.com>
2025-03-07 15:18:44 +0800

    安装模型改为国内源

--- installer/install_model.py
+++ installer/install_model.py
@@ -50,7 +50,7 @@ model_config = [
     },
     {
         'download_params': {
-            'urls': ["https://huggingface.co/shibing624/text2vec-base-chinese/tree/main"],
+            'urls': ["https://hf-mirror.com/shibing624/text2vec-base-chinese/tree/main"],
             'file_save_paths': [os.path.join(prefix_dir, 'embedding',"shibing624_text2vec-base-chinese")]
         },
         'download_function': hg.get_batch_data
